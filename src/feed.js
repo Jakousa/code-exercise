3656cba2a22bc1d7f6bda8dc841225611a1e8ed8
Jakousa <jakousa@cs.helsinki.fi>
2018-04-15 22:11:14 +0300

    try solving with streams

--- src/feed.js
+++ src/feed.js
@@ -1,7 +1,27 @@
 const Rx = require('rx-node');
+const { Readable } = require('stream')
 
+const remappedTypes = {
+  add: 'insert',
+  change: 'update'
+}
 // Export a function that wraps any database query changefeed into
 // RX observable
 module.exports = (queryObject) => {
-  // TODO: Return observable
+  const readable = new Readable({
+    read(size) {}
+  })
+  queryObject.changes({ includeTypes: true }).run((err, muuta) => {
+    muuta.each((err, message) => {
+      if (!err) {
+        const formattedMessage = {
+          type: remappedTypes[message.type],
+          prev: message.old_val,
+          next: message.new_val,
+        }
+        readable.push(JSON.stringify(formattedMessage))
+      }
+    })
+  })
+  return Rx.fromReadableStream(readable, 'data')
 };
